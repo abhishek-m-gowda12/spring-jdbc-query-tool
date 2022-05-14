6e9350fdd47db58e7aa1665397a72b10ffde5715
Abhishekgowda <abhishek@emproto.com>
2022-05-14 22:47:00 +0530

    initial commit

---
+++ src/main/java/com/abhishek/springjdbcquerytool/SpringJdbcQueryToolApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.springjdbcquerytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcQueryToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcQueryToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhishek/springjdbcquerytool/SpringJdbcQueryToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.springjdbcquerytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcQueryToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
